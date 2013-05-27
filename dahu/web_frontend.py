e1269ae6482d3a2bdea01563a5c257d02b930c1e
Damien Riquet <damien.riquet@lifl.fr>
2013-05-27 11:43:16 +0200

    Using a server-based session extension

--- dahu/web_frontend.py
+++ dahu/web_frontend.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from simplekv.memory import DictStore
+from flaskext.kvsession import KVSessionExtension
 from frontend.views import frontend
 
+# App related initialization
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-
 app.register_blueprint(frontend, url_prefix=app.config['FRONTEND_PREFIX'])
+
+# Session KVSession
+session_store = DictStore()
+KVSessionExtension(session_store, app)
 
 if __name__ == '__main__':
     app.debug = True
